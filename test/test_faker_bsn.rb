f1319311636183ccbe7debd3e8ef02793fb5de92
Dunya Kirkali <dunyakirkali@gmail.com>
2015-04-23 10:23:33 +0200

    add test for elfproef

--- test/test_faker_bsn.rb
+++ test/test_faker_bsn.rb
@@ -2,7 +2,13 @@ require File.expand_path(File.dirname(__FILE__) + '/test_helper.rb')
 
 class TestFakerBsn < Test::Unit::TestCase
 
-  def test_bsn_should_have_length_of_eight_or_nine
+  def test_bsn_length
     assert [8,9].include?(Faker::Bsn.bsn.size)
+  end
+
+  def test_bsn_validity
+    bsn = Faker::Bsn.bsn
+    elfproef = bsn.to_i
+    assert elfproef % 11 == 0
   end
 end
