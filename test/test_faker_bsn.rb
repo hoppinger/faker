0abe64b816aad5a8a11f6652dcdf1ff2c04c89d3
Dunya Kirkali <dunyakirkali@gmail.com>
2015-04-23 10:21:00 +0200

    implemented length test for fake bsn

--- test/test_faker_bsn.rb
+++ test/test_faker_bsn.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/test_helper.rb')
 
 class TestFakerBsn < Test::Unit::TestCase
 
-  def bsn_should_have_length_of_eight_or_nine
-    assert [8,9].include?(Faker::Bsn.bsn.length)
+  def test_bsn_should_have_length_of_eight_or_nine
+    assert [8,9].include?(Faker::Bsn.bsn.size)
   end
 end
