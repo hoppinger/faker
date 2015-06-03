27eb4bdbbab51ae6a0a73e683da4608fdc48ed97
Marthyn <Marthyn@live.nl>
2015-06-03 13:47:11 +0200

    Add length 9 and make more descriptive

--- lib/faker/bsn.rb
+++ lib/faker/bsn.rb
@@ -5,17 +5,29 @@ module Faker
       # returns a valid bsn number of length 8 or 9
       def bsn
         loop do
-          bsn = ''
-          total = (0..8).inject do |total, i|
+          bsn = []
+          total = number_list.inject do |total, i|
             random_number = random_for(i)
-            bsn += random_number.to_s
+            bsn << random_number
             total += random_number * (i == 8 ? -1 : (9 - i))
           end
-          return bsn.to_i if total % 11 == 0
+          return parse(bsn) if dividable_by_eleven(total)
         end
       end
 
       private
+
+      def number_list
+        [(0..8), (0..9)][rand(2)]
+      end
+
+      def parse bsn
+        bsn.to_s.to_i
+      end
+
+      def dividable_by_eleven n
+        n % 11 == 0
+      end
 
       def random_for(i)
         rand(i == 0 ? 2 : 9)
