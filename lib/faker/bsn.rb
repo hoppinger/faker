3d2cabfecb6a751a9ecdf0314d67642fd84bfe0a
Marthyn <Marthyn@live.nl>
2015-06-03 09:38:49 +0200

    Use inject, less lines and more descriptive

--- lib/faker/bsn.rb
+++ lib/faker/bsn.rb
@@ -5,16 +5,14 @@ module Faker
       # returns a valid bsn number of length 8 or 9
       def bsn
         loop do
-          # TODO: (dunyakirkali) use inject
           bsn = ''
-          total = 0
-          (0..8).each do |i|
-            rnd = random_for(i)
-            total += rnd * (i == 8 ? -1 : (9 - i))
-            bsn += rnd.to_s
+          total = (0..8).inject do |total, i|
+            random_number = random_for(i)
+            bsn += random_number.to_s
+            total += random_number * (i == 8 ? -1 : (9 - i))
           end
-          bsn.concat(total % 11)
-          return bsn.to_i if total % 11 == 0
+          elf_proof = total % 11
+          return bsn.concat(elf_proof).to_i if elf_proof == 0
         end
       end
 
