6262442b45d3d20b85068d4bef733338d682ca77
Marthyn <Marthyn@live.nl>
2015-06-03 14:13:38 +0200

    add correct size spec

--- lib/faker/bsn.rb
+++ lib/faker/bsn.rb
@@ -5,24 +5,21 @@ module Faker
       # returns a valid bsn number of length 8 or 9
       def bsn
         loop do
-          bsn = []
-          total = number_list.inject do |total, i|
+          bsn = ""
+          numbers = zero_to_eight_or_nine_list
+          total = (numbers).inject do |total, i|
             random_number = random_for(i)
-            bsn << random_number
-            total += random_number * (i == 8 ? -1 : (9 - i))
+            bsn += random_number.to_s
+            total += random_number * (i == numbers.last ? -1 : ((numbers.last+1) - i))
           end
-          return parse(bsn) if dividable_by_eleven(total)
+          return bsn.to_i if dividable_by_eleven(total)
         end
       end
 
       private
 
-      def number_list
+      def zero_to_eight_or_nine_list
         [(0..8), (0..9)][rand(2)]
-      end
-
-      def parse bsn
-        bsn.to_s.to_i
       end
 
       def dividable_by_eleven n
--- test/test_faker_bsn.rb
+++ test/test_faker_bsn.rb
@@ -2,12 +2,15 @@ require File.expand_path(File.dirname(__FILE__) + '/test_helper.rb')
 
 class TestFakerBsn < Test::Unit::TestCase
   def test_bsn_length
-    assert [8,9].include?(Faker::Bsn.bsn.size)
+    bsn = Faker::Bsn.bsn
+    assert [8,9].include? bsn.to_s.length
   end
 
   def test_bsn_validity
-    bsn = Faker::Bsn.bsn
-    elfproef = bsn.to_s.reverse.chars.map.with_index(1) { |char, i| char.to_i * (i == 1 ? -1 : i) }.inject{ |sum, x| sum + x }
-    assert elfproef % 11 == 0
+    100.times do
+      bsn = Faker::Bsn.bsn
+      elfproef = bsn.to_s.reverse.chars.map.with_index(1) { |char, i| char.to_i * (i == 1 ? -1 : i) }.inject{ |sum, x| sum + x }
+      assert elfproef % 11 == 0
+    end
   end
 end
